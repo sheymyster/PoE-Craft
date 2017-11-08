d718a7608a65f3950205564cd9253424d8166780
sheymyster <sheysjunk@gmail.com>
2017-11-08 10:28:50 -0600

    Added base code
    added base code

---
+++ src/js/containers/item-affixs.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {selectUser} from '../actions/index'
+
+
+class ItemAffixs extends Component {
+
+    renderList() {
+        return this.props.affixs.map((affix) => {
+            return (
+                <li>
+                    {affix.text1} {affix.roll1} {affix.text2} {affix.tier}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.renderList()}
+            </ul>
+        );
+    }
+
+}
+
+// Get apps state and pass it as props to UserList
+//      > whenever state changes, the UserList will automatically re-render
+function mapStateToProps(state) {
+    return {
+        affixs: state.affixs
+    };
+}
+
+// Get actions and pass them as props to to UserList
+//      > now UserList has this.props.selectUser
+function matchDispatchToProps(dispatch){
+    return bindActionCreators({selectUser: selectUser}, dispatch);
+}
+
+// We don't want to return the plain UserList (component) anymore, we want to return the smart Container
+//      > UserList is now aware of state and actions
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
